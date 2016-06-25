46bae6d82887140c21de9fef96441c9fd7b73cba
romanceegas <romanceegas@gmail.com>
2016-06-25 16:05:10 +0200

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create ydef post_list(request):
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+    
